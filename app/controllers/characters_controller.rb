0ff5314458e494b2d39800d038a005599b4d33ba
zcammack <zack.cammack@gmail.com>
2020-11-09 21:45:50 -0600

    Fixed the original form tag. Cleaned up code a bit.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,6 +4,15 @@ class CharactersController < ApplicationController
     end
 
     def create
-        
+        @character = Character.new(character_params)
+
+        @character.save
+        redirect_to @character
     end
+
+    private
+
+        def character_params
+            params.require(:character).permit(:first_name, :last_name)
+        end
 end
