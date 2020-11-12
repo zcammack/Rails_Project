b8be3ef96bb67cea04d7b89f0b449a4e6bbe44f4
zcammack <zack.cammack@gmail.com>
2020-11-12 05:35:57 -0600

    Fixed a lot of views and created the basic views for the campaigns model. It is similar to the character model for now.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,10 +1,54 @@
 class CampaignsController < ApplicationController
 
+    before_action :get_campaign, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @campaigns = Campaign.all
+    end
+
+    def show
+
+    end
+
+    def new
+        @campaign = Campaign.new
+    end
+
+    def edit
+        get_campaign
+    end
+
+    def create
+        @campaign = Campaign.new(campaign_params)
+
+        if @campaign.save
+            redirect_to @campaign
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        if @campaign.update(campaign_params)
+            redirect_to @campaign
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @campaign.destroy
+        redirect_to campaigns_path
+    end
 
 
     private
 
+    def get_campaign
+        @campaign = Campaign.find(params[:id])
+    end
+
     def campaign_params
-        params.require(:campaign).permit(:)
+        params.require(:campaign).permit(:title, :description, :setting, :dungeonmaster)
     end
 end
