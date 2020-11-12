e4fd84daa81b9eca8b8cf60a7f53b106614c2873
zcammack <zack.cammack@gmail.com>
2020-11-12 12:51:57 -0600

    Created associates between models. Characters will act as a join table for users and campaigns, as a user shouldn't be able to make a character with no campaign.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ApplicationRecord
     belongs_to :campaign
+    belongs_to :user
     validates :first_name, presence: true
     validates :last_name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  has_many :characters
+  has_many :campaigns through: :characters
 end
