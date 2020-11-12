bdad44bb123d53b9d897a2dca724909cd286936b
zcammack <zack.cammack@gmail.com>
2020-11-12 02:35:24 -0600

    Created nested resouce. Characters will be nested (partially) inside of campaign. This is because a campaign can be made without characters, but a character must be assigned to a campaign.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :characters
+  resources :campaigns do
+    resources :characters, shallow: true
+    #This nestes only the index, new, and create actions.
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
