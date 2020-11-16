b89ee27dcba9bf95f5565d625f3dfc330be75358
zcammack <zack.cammack@gmail.com>
2020-11-16 15:10:01 -0600

    Added dungeonmaster relationships and a simple validation for name.

--- app/models/dungeonmaster.rb
+++ app/models/dungeonmaster.rb
@@ -1,14 +1,9 @@
 class Dungeonmaster < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
-  has_many( 
-    :campaigns,
-    class_name: 'Campaign',
-    foreign_key: 'dungeonmaster_id',
-    inverse_of: :creator
-  )
-  has_many :users, through: :campaigns
+  belongs_to :user
+  has_many :campaigns
+  has_many :partys, through: :campaigns
+  has_many :players, through: :partys
+  validates :name, presence: true
+
 end
