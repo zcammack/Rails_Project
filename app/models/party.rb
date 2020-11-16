f1cc70d37fc43bd86ac3791589ae4cc09aa15d6a
zcammack <zack.cammack@gmail.com>
2020-11-16 14:57:55 -0600

    Added relationships and some validations to Party.

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,2 +1,12 @@
 class Party < ApplicationRecord
+    belongs_to :campaign
+    has_many :players
+    has_many :characters
+    has_many :levels, through: :characters
+    validates :title, presence: true
+    validates_uniqueness_of :title, scope: :campaign_id
+    validates_numericality_of :size
+    accepts_nested_attributes_for :characters
+
+
 end
