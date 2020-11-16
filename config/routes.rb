00217079b365b6e2720d3f32180a30c0918b77b9
zcammack <zack.cammack@gmail.com>
2020-11-16 15:13:51 -0600

    Added routes for players. May need to change characters...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
+  resources :dungeonmasters do
+    resources :campaigns do
+      resources :characters
+    end
+  end
+  resources :players
+
+  devise_for :users, :controllers = > { :omniauth_callbacks => "users/omniauth_callbacks" }
   
-  
-  
-root 'home#index'
+  root 'home#index'
 end
