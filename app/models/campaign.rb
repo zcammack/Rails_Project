e37b7ffc386ba141bdb332fb13de3220bbcb1ed1
zcammack <zack.cammack@gmail.com>
2020-11-16 15:05:08 -0600

    Added to campaign model. Attempted to create a scope method as well to allow a DM to see their parties from biggest to smallest.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,10 +1,11 @@
 class Campaign < ApplicationRecord
-    has_many :characters
-    belongs_to :user
-    belongs_to( 
-        :creator,
-        class_name: 'Dungeonmaster',
-        foreign_key: 'dungeonmaster_id',
-        inverse_of: :campaigns
-    )
+    has_many :partys
+    belongs_to :dungeonmaster
+    accepts_nested_attributes_for :partys
+    validates :title, presence: true
+    validates_numericality_of :number_of_parties
+    validates :setting, presence: true
+    validates :description, presence: true
+
+    scope :biggest_to_smallest, -> { order("number_of_parties desc") }
 end
