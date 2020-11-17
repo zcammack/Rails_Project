ebc389b31361edf13397a94b4e0b45032d386309
zcammack <zack.cammack@gmail.com>
2020-11-17 16:20:02 -0600

    Added a way for characters to be added into Party objects.

--- app/models/party.rb
+++ app/models/party.rb
@@ -10,4 +10,14 @@ class Party < ApplicationRecord
 
     scope :parties_with_4_or_more, -> { where("party.size >= ?", 4)}
 
+
+    def characters_attributes=(character_attributes)
+		character_attributes.values.each do |character_attribute|
+		  if character_attribute[:first_name].present?
+		  	character = Character.find_or_create_by(character_attribute)
+		  	self.characters << character
+		  end
+		end
+	end
+
 end
