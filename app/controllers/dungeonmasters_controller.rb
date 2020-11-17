04f2bd26f2950744b17306fe2a11d63b84278793
zcammack <zack.cammack@gmail.com>
2020-11-17 16:26:49 -0600

    Created dungeonmaster controller.

---
+++ app/controllers/dungeonmasters_controller.rb
@@ -0,0 +1,42 @@
+class DungeonmastersController < ApplicationController
+
+    before_action :set_dungeonmaster, only: [:parties_with_4_or_more, :show]
+
+	def parties_with_4_or_more
+
+	end
+
+	def index
+		@dungeonmasters = Dungeonmaster.all
+	end
+
+	def new
+		@dungeonmaster = Dungeonmaster.new
+	end
+
+	def create
+		@dungeonmaster = dungeonmaster.new(name: dungeonmaster_params[:name], user: current_user)
+		if @dungeonmaster.save
+			@dungeonmaster.user.set_dungeonmaster_role
+			redirect_to dungeonmaster_path(@dungeonmaster)
+		else 
+			flash[:notice] = "The dungeonmaster couldn't be saved"
+			render :new
+		end
+	end
+
+	def show
+		@campaigns = @dungeonmaster.campaigns.all
+	end
+
+    private
+
+    def set_dungeonmaster
+        @dungeonmaster = Dungeonmaster.find(params[:id])
+    end
+    
+
+	def dungeonmaster_params
+		params.require(:dungeonmaster).permit(:name)
+	end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
-    def welcome
+    def home
         
     end
 end
