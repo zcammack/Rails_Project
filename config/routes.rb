ce576c513fc4ddca2e1928b958a85c33a4473d3e
zcammack <zack.cammack@gmail.com>
2020-11-16 19:03:17 -0600

    Created Party controller. Fixed typo in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   resources :players
 
-  devise_for :users, :controllers = > { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
   root 'home#index'
 end
