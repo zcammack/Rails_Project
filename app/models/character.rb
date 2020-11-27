60bce99f1e1a85a38afb722cd1c45efea05d861e
zcammack <zack.cammack@gmail.com>
2020-11-27 11:37:41 -0600

    Testing a commit to github.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,7 @@
 class Character < ApplicationRecord
     belongs_to :party
     belongs_to :user
+    has_many :campaigns, through: :parties
     validates :name, presence: true
     validates :level, presence: true, numericality: true
     validates :profession, presence: true
